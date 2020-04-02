5a2cfea659c64154afbcea93cb003e6943b922db
bmtende <60487102+bmtende@users.noreply.github.com>
2020-04-02 12:43:10 -0400

    Update main.js
    Created a for loop that should loop through the 5 pictures

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -5,9 +5,11 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+for(let i=1; i <= 5; i++){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'Images/pic' + i + '.png');
+    thumbBar.appendChild(newImage);
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
