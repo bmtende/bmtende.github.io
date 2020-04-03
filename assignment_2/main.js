8d80a989e9c6ed9c34325419d173f17bdefe9774
bmtende <60487102+bmtende@users.noreply.github.com>
2020-04-02 22:52:52 -0400

    Update main.js
    add handlers

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -10,15 +10,25 @@ for(let i=1; i <= 5; i++){
     newImage.setAttribute('src', 'Images/pic' + i + '.png');
     thumbBar.appendChild(newImage);
     newImage.onclick = function(){
-        displayedImage.src = newImage.src
+        displayedImage.src = newImage.src;
     }
-    
-
 }
 
-
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function(){
+    const newbtn = btn.getAttribute('class');
+    if(newbtn = 'dark'){
+        btn.setAttribute('class','light');
+        btn.textContent('Lighten');
+        overlay.style.backgroundColor = 'gba(0,0,0,0.5)';
+    }
+    else{
+        btn.setAttribute('class','dark');
+        btn.textContent('Darken');
+        overlay.style.backgroundColor = 'gba(0,0,0,0)';
+    }
 
+}
 
 
 
